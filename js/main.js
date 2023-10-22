faa54cde7574f2455e54f01744a0e8a527e90466
M0stafa123 <jodat.ahmed135@gmail.com>
2023-10-23 01:02:34 +0300

    upload

---
+++ js/main.js
@@ -0,0 +1,37 @@
+let prog = document.querySelectorAll(".skill .prog");
+let ourSkill = document.getElementById("our-skills");
+
+window.onscroll = function () {
+  if (window.scrollY >= ourSkill.offsetTop - 300) {
+    prog.forEach((e) => (e.style.width = e.dataset.width));
+  }
+};
+
+//  ###############
+let stat = document.getElementById("stats");
+let p = document.querySelectorAll("#stats p");
+let started = false;
+window.onscroll = function () {
+  if (window.scrollY >= stat.offsetTop - 400) {
+    if (!started) {
+      p.forEach((e) => counter(e));
+    }
+    started = true;
+  }
+};
+function counter(el) {
+  let a = setInterval(() => {
+    el.innerHTML++;
+    if (el.innerHTML == el.dataset.count) {
+      clearInterval(a);
+    }
+  }, 2000 / el.dataset.count);
+}
+//  ################
+let body = document.body;
+let scrollProg = document.querySelector(".scroll-prog");
+window.onscroll = function () {
+  scrollProg.style.width = `${
+    (window.scrollY / (body.scrollHeight - +window.innerHeight)) * 100
+  }%`;
+};
